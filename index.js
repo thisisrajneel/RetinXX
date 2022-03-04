8f9ef050696d479ef02480cd07239b69bdbe0316
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-04 16:13:24 +0530

    found codepen

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let today = new Date();
 const text = document.getElementById("text");
 
 let timer = new Date(
-    today.getFullYear(),today.getMonth(),today.getDay(),today.getHours(),today.getMinutes() + 20,today.getSeconds()+1,today.getMilliseconds()
+    today.getFullYear(),today.getMonth(),today.getDay(),today.getHours(),today.getMinutes() + 0,today.getSeconds()+10,today.getMilliseconds()
 )
 
 const msDiff = timer - today
@@ -11,9 +11,9 @@ const setTimer = setInterval(twentyTimer = () => {
 
     let now = new Date()
     let diff = timer - now
-    // if(diff < 0) {
-    //     clearInterval(setTimer)
-    // }
+    if(diff <= 0) {
+        location.reload();
+    }
 
     let min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     let sec = Math.floor((diff % (1000 * 60)) / 1000);
