ca1942988375cba895070e564d453b46259b0381
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-03-04 15:54:22 +0530

    20m timer w/o base condition

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const timer = setInterval(() => {
-  let now = new Date();
+let today = new Date();
+const text = document.getElementById("text");
 
-  let hr = String(now.getHours());
-  let min = String(now.getMinutes());
-  let sec = String(now.getSeconds());
-  let ms = String(now.getMilliseconds());
+let timer = new Date(
+    today.getFullYear(),today.getMonth(),today.getDay(),today.getHours(),today.getMinutes() + 20,today.getSeconds()+1,today.getMilliseconds()
+)
 
-  let time = hr + " h " + min + " min " + sec + " s " + ms + " ms";
-  // console.log(time);
-  const text = document.getElementById("text");
+const msDiff = timer - today
+
+const setTimer = setInterval(twentyTimer = () => {
+
+    let now = new Date()
+    let diff = timer - now
+    // if(diff < 0) {
+    //     clearInterval(setTimer)
+    // }
+
+    let min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let sec = Math.floor((diff % (1000 * 60)) / 1000);
+    let ms = Math.floor((diff % (1000 * 60)));
+
+  let time = min + " min " + sec + " s";
+  
   text.innerHTML = time;
 }, 1);
